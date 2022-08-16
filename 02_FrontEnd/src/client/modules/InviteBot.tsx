dd75c273e6326c3358ae7497615f970a7fa15bfc
Vedhagiri <giri@recosenselabs.com>
2022-08-16 15:11:37 +0530

    added german language

--- 02_FrontEnd/src/client/modules/InviteBot.tsx
+++ 02_FrontEnd/src/client/modules/InviteBot.tsx
@@ -45,7 +45,7 @@ const InviteBot = (props) =>
 
     const myLanguages: LanguageEntity[] = [        
         { key: 'en-US', text: 'English' },
-        { key: 'el-GR', text: 'Greek' },   
+        { key: 'de-DE', text: 'German' },   
         { key: 'pl-PL', text: 'Polish' },
         { key: 'ru-RU', text: 'Russian' },
         { key: 'sl-SI', text: 'Slovenian' },
