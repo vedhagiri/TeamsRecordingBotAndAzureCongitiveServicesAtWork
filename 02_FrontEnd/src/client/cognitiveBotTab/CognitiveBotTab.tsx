12571ceea0aff7ff61bd13a0f6475029b1176b22
Vedhagiri <giri@recosenselabs.com>
2022-09-27 11:32:42 +0530

    recording bot changes

--- 02_FrontEnd/src/client/cognitiveBotTab/CognitiveBotTab.tsx
+++ 02_FrontEnd/src/client/cognitiveBotTab/CognitiveBotTab.tsx
@@ -8,63 +8,76 @@ import MyTranscriptions from "../modules/MyTranscriptions";
 import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
 import { reactPlugin } from "../modules/AppInsights";
 import InviteBot from "../modules/InviteBot";
+import styles from "../Styles/styles.module.css";
 
 /**
  * Implementation of the Cognitive Bot content page
  */
 export const CognitiveBotTab = () => {
+  const [{ inTeams, theme, context }] = useTeams();
+  const [currentCallId, setcurrentCallId] = useState<string>();
+  const [inMeeting, setinMeeting] = useState<boolean>();
+  const [currentJoinUrl, setcurrentJoinUrl] = useState<string>();
 
-    const [{ inTeams, theme, context }] = useTeams();    
-    const [currentCallId, setcurrentCallId] = useState<string>();    
-    const [inMeeting, setinMeeting] = useState<boolean>();
-    const [currentJoinUrl, setcurrentJoinUrl] = useState<string>();    
+  useEffect(() => {
+    if (inTeams === true) {
+      microsoftTeams.appInitialization.notifySuccess();
 
-    useEffect(() => {
-        if (inTeams === true) {
-            microsoftTeams.appInitialization.notifySuccess();
+      // Get Join Meeting Url
+      if (
+        context &&
+        context.meetingId &&
+        context.chatId &&
+        context.tid &&
+        context.userObjectId
+      ) {
+        console.log("In Teams Meeting, meeting id is: " + context.meetingId);
+        console.log("Context is: " + JSON.stringify(context));
+        setinMeeting(true);
 
-            // Get Join Meeting Url
-            if(context && context.meetingId && context.chatId && context.tid && context.userObjectId)
-            {
-                console.log("In Teams Meeting, meeting id is: " + context.meetingId);
-                console.log("Context is: " + JSON.stringify(context));
-                setinMeeting(true);
+        // Try to create Join Url
+        let lJoinUrl =
+          'https://teams.microsoft.com/l/meetup-join/CHAT_ID/0?context={"Tid":"T_ID","Oid":"O_ID"}'
+            .replace("CHAT_ID", context.chatId)
+            .replace("T_ID", context.tid)
+            .replace("O_ID", context.userObjectId);
 
-                // Try to create Join Url
-                let lJoinUrl = "https://teams.microsoft.com/l/meetup-join/CHAT_ID/0?context={\"Tid\":\"T_ID\",\"Oid\":\"O_ID\"}".
-                replace("CHAT_ID", context.chatId).
-                replace("T_ID", context.tid).
-                replace("O_ID", context.userObjectId);
+        console.log("Join web url is: " + lJoinUrl);
 
-                console.log("Join web url is: " + lJoinUrl);
+        setcurrentJoinUrl(lJoinUrl);
+      }
+    }
+  }, [inTeams]);
 
-                setcurrentJoinUrl(lJoinUrl);
-            }
+  /**
+   * The render() method to create the UI of the tab
+   */
+  return (
+    <AppInsightsContext.Provider value={reactPlugin}>
+      <Provider theme={theme}>
+        <Flex column fill={true}>
+          {inMeeting && !currentCallId ? (
+            <InviteBot
+              currentJoinUrl={currentJoinUrl}
+              setcurrentCallId={setcurrentCallId}
+            />
+          ) : null}
 
-        }
-    }, [inTeams]);     
-
-    /**
-     * The render() method to create the UI of the tab
-     */
-    return (
-        <AppInsightsContext.Provider value={reactPlugin}>
-            <Provider theme={theme}>
-                <Flex column fill={true}>
-
-                    {inMeeting && !currentCallId ?                        
-                        ( <InviteBot currentJoinUrl={currentJoinUrl} setcurrentCallId={setcurrentCallId}  />) :
-                        (null)
-                    }
-
-                    {!inMeeting ? 
-                        (<MyCalls setcurrentCallId={setcurrentCallId} />) :
-                        (null)
-                    }
-
-                    <MyTranscriptions currentCallId={currentCallId} />
-                </Flex>
-            </Provider>
-        </AppInsightsContext.Provider>        
-    );
+          {!inMeeting ? <MyCalls setcurrentCallId={setcurrentCallId} /> : null}
+          <div style={{ position: "relative" }}>
+            {inMeeting ? (
+              <div className={styles.recordingContainer}>
+                <span>
+                  <b className={styles.Blink}>&#11044;</b>
+                  &nbsp;
+                  <b style={{ color: "#ffffff" }}>&nbsp;Recording.</b>
+                </span>
+              </div>
+            ) : null}
+            <MyTranscriptions currentCallId={currentCallId} />
+          </div>
+        </Flex>
+      </Provider>
+    </AppInsightsContext.Provider>
+  );
 };
--- 02_FrontEnd/webpack.config.js
+++ 02_FrontEnd/webpack.config.js
@@ -1,4 +1,4 @@
-// Copyright (c) Wictor Wilén. All rights reserved. 
+// Copyright (c) Wictor Wilén. All rights reserved.
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
@@ -14,77 +14,104 @@ const argv = require("yargs").argv;
 const debug = argv.debug !== undefined;
 const lint = !(argv["no-linting"] || argv.l === true);
 
-const config = [{
+const config = [
+  {
     entry: {
-        server: [
-            path.join(__dirname, "/src/server/server.ts")
-        ]
+      server: [path.join(__dirname, "/src/server/server.ts")],
     },
     mode: debug ? "development" : "production",
     output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "[name].js",
-        devtoolModuleFilenameTemplate: debug ? "[absolute-resource-path]" : "[]"
+      path: path.join(__dirname, "/dist"),
+      filename: "[name].js",
+      devtoolModuleFilenameTemplate: debug ? "[absolute-resource-path]" : "[]",
     },
     externals: [nodeExternals()],
     devtool: debug ? "source-map" : "source-map",
     resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        alias: {}
+      extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+      alias: {},
     },
     target: "node",
     node: {
-        __dirname: false,
-        __filename: false
+      __dirname: false,
+      __filename: false,
     },
     module: {
-        rules: [{
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: ["ts-loader"]
-        }]
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: ["ts-loader"],
+        },
+      ],
     },
-    plugins: []
-},
-{
+    plugins: [],
+  },
+  {
     entry: {
-        client: [
-            path.join(__dirname, "/src/client/client.ts")
-        ]
+      client: [path.join(__dirname, "/src/client/client.ts")],
     },
     mode: debug ? "development" : "production",
     output: {
-        path: path.join(__dirname, "/dist/web/scripts"),
-        filename: "[name].js",
-        libraryTarget: "umd",
-        library: "cognitiveBot",
-        publicPath: "/scripts/"
+      path: path.join(__dirname, "/dist/web/scripts"),
+      filename: "[name].js",
+      libraryTarget: "umd",
+      library: "cognitiveBot",
+      publicPath: "/scripts/",
     },
     externals: {},
     devtool: debug ? "source-map" : "source-map",
     resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-        alias: {}
+      extensions: [".ts", ".tsx", ".js", ".css", ".scss"],
+      alias: {},
     },
     target: "web",
     module: {
-        rules: [{
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: ["ts-loader"]
-        }]
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+                modules: true,
+              },
+            },
+          ],
+          include: /\.module\.css$/,
+        },
+        // { test: /\.scss$/, use: [
+        //     { loader: "style-loader" },  // to inject the result into the DOM as a style block
+        //     { loader: "css-modules-typescript-loader"},  // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+        //     { loader: "css-loader", options: { modules: true } },  // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+        //     { loader: "sass-loader" },  // to convert SASS to CSS
+        //     // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
+        // ] },
+
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: ["ts-loader"],
+        },
+      ],
     },
     plugins: [
-        new Dotenv({
-            systemvars: true
-        })
-    ]
-}
+      new Dotenv({
+        systemvars: true,
+      }),
+    ],
+  },
 ];
 
 if (lint !== false) {
-    config[0].plugins.push(new ESLintPlugin({ extensions: ["ts", "tsx"], failOnError: false }));
-    config[1].plugins.push(new ESLintPlugin({ extensions: ["ts", "tsx"], failOnError: false }));
+  config[0].plugins.push(
+    new ESLintPlugin({ extensions: ["ts", "tsx"], failOnError: false })
+  );
+  config[1].plugins.push(
+    new ESLintPlugin({ extensions: ["ts", "tsx"], failOnError: false })
+  );
 }
 
 module.exports = config;
